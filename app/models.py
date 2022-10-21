dd9f6ee04543d8aa800871140fca5dabfc209b47
Cooper McVay <cmcvay23@basecampcodingacademy.org>
2022-10-21 13:15:22 -0500

    COMMIT!

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Purchase(models.Model):
+    name = models.TextField()
+    phone = models.TextField()
+    town = models.TextField()
+    
+def profile_create(name,phone,town):
+    newp = Purchase(name = name, phone = phone, town = town)
+    newp.save()
+    return newp
+
+def all_profiles():
+    return Purchase.objects.all()
+
+def find_by_name(customer):
+    return Purchase.objects.get(name = customer)
+
+def find_by_town(town):
+    f = Purchase.objects.filter(name = town)
+    if len(f) == 0:
+        return None
+    return Purchase.objects.get(name = town)
+
+def delete(name):
+    f = Purchase.objects.filter(name = name)
+    if len(f) == 0:
+        return None
+    return Purchase.objects.filter(name = name).delete()
